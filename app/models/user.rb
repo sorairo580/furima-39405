25385f1ad4444189b8090e0ce9aed88f66d0833b
sorairo580 <137414891+sorairo580@users.noreply.github.com>
2023-09-13 00:56:24 +0900

    ユーザー管理機能１
    ブランチを作成、Deviseを導入、ユーザー管理機能を実装のうちバリデーション（新規登録/ユーザー情報、新規登録/本人情報確認）、トップページのヘッダーに「新規登録」「ログイン」ボタン

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email,    presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :encrypted_password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/}, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  validates :last_name, presence: true, format: { with: /\A[\p{Hiragana}\p{Katakana}\p{Han}ー－]+\z/u}
+  validates :first_name, presence: true, format: { with: /\A[\p{Hiragana}\p{Katakana}\p{Han}ー－]+\z/u}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/u}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/u}
+  validates :birthday, presence: true
+
+end
