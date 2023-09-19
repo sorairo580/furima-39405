a92c0cbbf80019eb18448e457527da99fc6acbeb
sorairo580 <137414891+sorairo580@users.noreply.github.com>
2023-09-20 03:43:44 +0900

    商品出品機能first commit
    ①itemテーブルを作成
    ②newアクションを作成
    ③Active Storageの導入
    ④itemモデルに画像ファイルの紐づけ
    ⑤画像保存許可のストロングパラメーター

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :purchase_record
+  has_one_attached :image
+
+  validates :text, presence: true
+
 end
--- db/migrate/20230904133132_create_items.rb
+++ db/migrate/20230904133132_create_items.rb
@@ -1,7 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      
+      t.string    :item_name,         null: false, default: ""
+      t.integer   :category_id,       null: false
+      t.integer   :price,             null: false
+      t.text      :item_description,  null: false
+      t.integer   :condition_id,      null: false
+      t.integer   :cost_id,           null: false
+      t.integer   :prefecture_id,     null: false
+      t.integer   :delivery_day_id,   null: false
       t.timestamps
     end
   end
