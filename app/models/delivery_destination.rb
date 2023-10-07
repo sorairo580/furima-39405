20c514f12cce4e2c2d1cdddec7a490472c00ccc7
sorairo580 <137414891+sorairo580@users.noreply.github.com>
2023-10-07 16:20:26 +0900

    DeliveryDestinationモデル編集

--- app/models/delivery_destination.rb
+++ app/models/delivery_destination.rb
@@ -1,3 +1,17 @@
 class DeliveryDestination < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :zip_cord, :prefecture_id, :city, :house_number, :building_name, :phone_number
+
+  validates :zip_cord,        presence: true
+  validates :prefecture_id,   presence: true
+  validates :city,            presence: true
+  validates :house_number,    presence: true
+  validates :building_name,   presence: true
+  validates :phone_number,    presence: true
+
+  def save
+  end
+
   belongs_to :purchase_record
+  
 end
