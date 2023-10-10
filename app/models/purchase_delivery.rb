32d84887de69f019d540b29baf89d620551ddbd1
sorairo580 <137414891+sorairo580@users.noreply.github.com>
2023-10-11 04:53:13 +0900

    不要な記述のコメントアウト

--- app/models/purchase_delivery.rb
+++ app/models/purchase_delivery.rb
@@ -1,33 +1,35 @@
 class PurchaseDelivery
   include ActiveModel::Model
-  attr_accessor :image, :item_name, :price, :cost_id, :card_number, :card_expiry_month, :card_expiry_year, :card_cvc, :user_id, :item_id, :zip_cord,
+  attr_accessor  :card_number, :card_expiry_month, :card_expiry_year, :card_cvc, :user_id, :item_id, :zip_cord,
    :prefecture_id, :city, :house_number, :building_name, :phone_number
 
   with_options presence: true do
-    validates :image
-    validates :item_name
-    validates :price,   numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999_999, message: 'is invalid' }
-    validates :cost_id,   numericality: { other_than: 1, message: "can't be blank" }
+    # validates :image
+    # validates :item_name
+    # validates :price,   numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999_999, message: 'is invalid' }
+    # validates :cost_id,   numericality: { other_than: 1, message: "can't be blank" }
     validates :card_number
     validates :card_expiry_month
     validates :card_expiry_year
     validates :card_cvc
     validates :user_id
-    validates :item_id
+    validates :item_id, numericality: { only_integer: true, greater_than_or_equal_to: 1, message: 'is invalid' }
   end
   with_options presence: true do
-    validates :zip_cord,   format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :zip_cord, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid' }
     validates :prefecture_id,   numericality: { other_than: 1, message: "can't be blank" }
-    validates :city     
-    validates :house_number    
-    validates :building_name
-    validates :phone_number
+    validates :city      
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
   end
 
   def save
-    @item = PurchaseRecord.new(image: image, item_name: item_name, price: price, cost_id: cost_id, user_id: user_id, item_id: item_id)
+    @item = PurchaseRecord.new(user_id: user_id, item_id: item_id)
     DeliveryDestination.new(zip_cord: zip_cord, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name,
        phone_number: phone_number, purchase_record_id: purchase_record_id)
   end
 
 end
+
+# :image, :item_name, :price, :cost_id,
+# image: image, item_name: item_name, price: price, cost_id: cost_id, 
--- app/models/purchase_record.rb
+++ app/models/purchase_record.rb
@@ -3,6 +3,6 @@ class PurchaseRecord < ApplicationRecord
   has_one    :delivery_destination
   belongs_to :item
   belongs_to :user
-  has_one_attached :image
+  # has_one_attached :image
  
 end
