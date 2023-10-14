169a14c8fc1efe8447e25aefb737e4f9babbdc4d
sorairo580 <137414891+sorairo580@users.noreply.github.com>
2023-10-14 12:36:06 +0900

    payjpの設定
    環境定数へ変更済み
    未ログイン状態で購入ページへ直接遷移しないよう修正

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,5 +1,5 @@
 class PurchaseRecordsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
   before_action :find_item 
 
   def index
@@ -12,6 +12,12 @@ class PurchaseRecordsController < ApplicationController
   def create
     @purchase_delivery = PurchaseDelivery.new(purchase_record_params)
       if @purchase_delivery.valid?
+        Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+        Payjp::Charge.create(
+        amount: purchase_record_params[:price], 
+        card: purchase_record_params[:token],   
+        currency: 'jpy'            
+      )
       @purchase_delivery.save
       return redirect_to root_path
     else
@@ -22,8 +28,8 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def purchase_record_params
-    params.require(:purchase_delivery).permit(:image, :item_name, :price, :cost_id, :zip_cord, :prefecture_id, :city, 
-      :house_number, :building_name, :phone_number).merge(user_id: params[:user_id], item_id: params[:item_id])
+    params.require(:purchase_delivery).permit(:card_number, :card_exp, :card_cvc, :image, :item_name, :price, :cost_id, :zip_cord, :prefecture_id, :city, 
+      :house_number, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 
   def find_item
@@ -31,3 +37,4 @@ class PurchaseRecordsController < ApplicationController
   end
 
 end
+
--- app/models/purchase_delivery.rb
+++ app/models/purchase_delivery.rb
@@ -1,7 +1,7 @@
 class PurchaseDelivery
   include ActiveModel::Model
-  attr_accessor  :card_number, :card_expiry_month, :card_expiry_year, :card_cvc, :user_id, :item_id, :zip_cord,
-   :prefecture_id, :city, :house_number, :building_name, :phone_number
+  attr_accessor  :card_number, :card_exp, :card_cvc, :user_id, :item_id, :zip_cord,
+   :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
 
   with_options presence: true do
     # validates :image
@@ -9,11 +9,11 @@ class PurchaseDelivery
     # validates :price,   numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999_999, message: 'is invalid' }
     # validates :cost_id,   numericality: { other_than: 1, message: "can't be blank" }
     validates :card_number
-    validates :card_expiry_month
-    validates :card_expiry_year
+    validates :card_exp
     validates :card_cvc
     validates :user_id
     validates :item_id, numericality: { only_integer: true, greater_than_or_equal_to: 1, message: 'is invalid' }
+    validates :token
   end
   with_options presence: true do
     validates :zip_cord, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid' }
@@ -30,6 +30,3 @@ class PurchaseDelivery
   end
 
 end
-
-# :image, :item_name, :price, :cost_id,
-# image: image, item_name: item_name, price: price, cost_id: cost_id, 
